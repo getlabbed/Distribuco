432e3ccc6ff622e0b8bc5b4067405176828c2330
GetLabbed <getlabbed@proton.me>
2023-04-01 04:00:39 -0400

    Added total slider display values

--- app/static/scripts/ajout.js
+++ app/static/scripts/ajout.js
@@ -2,7 +2,8 @@ function ajout() {
   const sliders = document.querySelectorAll('.slider');
   const cercles = document.querySelectorAll('.pourcentage');
   const cerclesForm = document.querySelectorAll('.pourcentageForm')
-  const inputmL = document.querySelectorAll('input[type="number"]');
+  const inputmL = document.querySelectorAll('.displayQty');
+  const displayTotal = document.querySelectorAll('.displayTotal')
   const total = document.querySelector('.slider-total')
   const maxVolume = 355
   let totalVolume = 355;
@@ -14,18 +15,18 @@ function ajout() {
     });
   
     sliders.forEach((slider, index) => {
-      const percentage = 100 * (parseInt(slider.value) / total);   // Calcul du pourcentage
-      const bar = slider.nextElementSibling.nextElementSibling;    // Get la barre qui est le prochain élément
-      bar.style.width = `${percentage}%`;                          // Appliquer le pourcentage aux barres
+      const percentage = 100 * (parseInt(slider.value) / total);     // Calcul du pourcentage
+      const bar = slider.nextElementSibling.nextElementSibling;      // Get la barre qui est le prochain élément
+      bar.style.width = `${percentage}%`;                            // Appliquer le pourcentage aux barres
   
       // Update du pourcentage dans le cercle
-      cercles[index].innerHTML = percentage.toFixed(1) + "%";      // Arrondir à 1 chiffre après la virgule, affecte aussi les entrées
+      cercles[index].innerHTML = percentage.toFixed(1) + "%";        // Arrondir à 1 chiffre après la virgule, affecte aussi les entrées
       cerclesForm[index].value = percentage.toFixed(1);
   
       // Update des input de quantitées de liquides
-      const volume = (percentage * totalVolume) / 100;             // Calcul du volume
-      inputmL[index * 2].value = volume;                           // 2 input à chaque ligne           (ml)
-      inputmL[index * 2 +1 ].value = volume * 0.033814;            // Décalé de 1 pour l'input de plus (oz)
+      const volume = (percentage * totalVolume) / 100;               // Calcul du volume
+      inputmL[index * 2].value = volume.toFixed(2);                  // 2 input à chaque ligne           (ml)
+      inputmL[index * 2 + 1].value = (volume * 0.033814).toFixed(2); // Décalé de 1 pour l'input de plus (oz)
     });
   }
   sliders.forEach(slider => {
@@ -44,6 +45,11 @@ function ajout() {
     const totalPercent = (knobPosition / totalSliderWidth) * 100;
 
     totalBar.style.width = `${totalPercent}%`;
+
+    displayTotal.forEach((display, index) => {
+      displayTotal[index * 2].value = totalVolume.toFixed(2);                 // (ml)
+      displayTotal[index * 2 + 1].value = (totalVolume * 0.033814).toFixed(2) // (oz)
+    });
     console.log(total.value, knobPosition, totalPercent);
     updatePercentages();
   });
