d72746810c46ec650613429eb4a06afa9a28c685
GetLabbed <getlabbed@proton.me>
2023-03-31 23:56:51 -0400

    Added Swup.js Page Animation

---
+++ app/static/scripts/PageAnimation.js
@@ -0,0 +1,6 @@
+// https://swup.js.org/
+
+// Initialisation de la bibliothèque Swup, qui permet de faire des animations entre les pages html
+const swup = new Swup({
+    animateHistoryBrowsing: true // Permet d'animer les pages avec l'historique
+  });
