32cdaa66426f78fe57d4df3705a79b56de2a50df
getlabbed@proton.me <1927941@cegepoutaouais.qc.ca>
2023-04-18 09:02:36 -0400

    feat: Added color input in form

--- app/static/scripts/colors.js
+++ app/static/scripts/colors.js
@@ -1,10 +1,18 @@
 // Code fait par chatGPT
 
-$('.color').colorPicker({
-    // Autres options
-    renderCallback: function($elm) {
-        var hexValue = this.color.colors.HEX; // Récupérer la valeur hexadécimale
-        // Afficher la valeur hexadécimale
-        $elm.val('#' + hexValue);
-    },
-});
+function color_wrapper(){
+    $('.color').colorPicker({
+        // Autres options
+        renderCallback: function($elm) {
+            var hexValue = this.color.colors.HEX; // Récupérer la valeur hexadécimale
+            // Afficher la valeur hexadécimale
+            $elm.val('#' + hexValue);
+        },
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', color_wrapper);     // Exécuter le code quand la page est chargée
+
+// Événements Swup https://swup.js.org/events
+document.addEventListener('swup:contentReplaced', color_wrapper); // Exécuter le code quand la page est chargée par Swup 
